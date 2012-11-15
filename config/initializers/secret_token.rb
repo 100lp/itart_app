594cab0c07002ee475fdf5a4b7a5a4b27adaf3b0
100lp <spb.gurme@gmail.com>
2012-11-15 13:03:19 +0400

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = 'ce8083b2766f7e05dfe50f07dfd09e5c4eebf8408af4b68f0aaa6a63054ce2c5a4719115983f7849c9c26f6ae1cf814cb674c0c1bbfcca0b250c735a37491287'
